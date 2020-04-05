212e7c183ea8195017773971eebce84e7b33a77f
Size <55809336+Size00@users.noreply.github.com>
2020-04-05 02:32:04 -0500

    Update get_mixerclips.php

--- get_mixerclips.php
+++ get_mixerclips.php
@@ -2,7 +2,6 @@ function mixerclips($id){
 $clips_jd = json_decode(file_get_contents('https://mixer.com/api/v1/clips/channels/'.$id), true);
 foreach($clips_jd as $clip){
 	echo 'https://mixer.com/Ice_Poseidon?clip='.$clip['shareableId'].'<br>';
-}
-
+ }
 }
 mixerclips('15759230'); // mixers user_id
